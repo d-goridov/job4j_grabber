b86b6de0a1fed03fc4b900b01fa79ad43cc28c9b
d.goridov <dimagoridov@yandex.ru>
2022-06-16 20:56:06 +0300

    2.4. SqlRuParse [#285213]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -1,38 +1,63 @@
 package ru.job4j.grabber;
 
-import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.utils.DateTimeParser;
+import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
-    public class HabrCareerParse {
-        private static final String SOURCE_LINK = "https://career.habr.com";
-        private static final String PAGE_LINK = String.format("%s/vacancies/java_developer?page=", SOURCE_LINK);
+public class HabrCareerParse implements Parse {
+    private static final String SOURCE_LINK = "https://career.habr.com";
+    private static final String PAGE_LINK = String.format("%s/vacancies/java_developer?page=", SOURCE_LINK);
+    private final DateTimeParser dateTimeParser;
 
-        private String retrieveDescription(String link) throws IOException {
-            Document document = Jsoup.connect(link).get();
-            Elements element = document.select(".style-ugc");
-            return element.text();
-        }
+    public HabrCareerParse(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
+    }
 
-        public static void main(String[] args) throws IOException {
-            for (int i = 1; i <= 5; i++) {
-                Connection connection = Jsoup.connect(PAGE_LINK + i);
-                Document document = connection.get();
-                Elements rows = document.select(".vacancy-card__inner");
-                rows.forEach(row -> {
-                    Element titleElement = row.select(".vacancy-card__title").first();
-                    Element dateElement = row.select(".vacancy-card__date").first().child(0);
-                    Element linkElement = titleElement.child(0);
-                    String vacancyName = titleElement.text();
-                    String vacancyDate = dateElement.attr("datetime");
-                    String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-                    System.out.printf("%s %s %s%n", vacancyName, vacancyDate, link);
-                });
+    private String retrieveDescription(String link) throws IOException {
+        Document document = Jsoup.connect(link).get();
+        Elements element = document.select(".style-ugc");
+        return element.text();
+    }
+
+    @Override
+    public List<Post> list(String link) throws IOException {
+        List<Post> list = new ArrayList<>();
+        Document document = Jsoup.connect(link).get();
+        Elements rows = document.select(".vacancy-card__inner");
+        rows.forEach(row -> {
+            Element titleElement = row.select(".vacancy-card__title").first();
+            Element linkElement = titleElement.child(0);
+            Element dateElement = row.select(".vacancy-card__date").first().child(0);
+            String title = titleElement.text();
+            String linkVacancy = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+            String description = "";
+            try {
+                description = retrieveDescription(linkVacancy);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
+
+            LocalDateTime dateTime = new HabrCareerDateTimeParser().parse(dateElement.attr("datetime"));
+            Post post = new Post(title, linkVacancy, description, dateTime);
+            list.add(post);
+        });
+        return list;
+    }
+
+    public static void main(String[] args) throws IOException {
+        HabrCareerDateTimeParser dateParser = new HabrCareerDateTimeParser();
+        HabrCareerParse careerParse = new HabrCareerParse(dateParser);
+        for (Post post : careerParse.list(PAGE_LINK)) {
+            System.out.println(post);
         }
     }
+}
 

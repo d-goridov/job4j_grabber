1e8e6bd7b64909a0cd16f915c05f676316625fa2
d.goridov <dimagoridov@yandex.ru>
2022-06-20 00:29:47 +0300

    5. PsqlStore [#285209]

---
+++ src/main/java/ru/job4j/grabber/PsqlStore.java
@@ -0,0 +1,120 @@
+package ru.job4j.grabber;
+
+import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class PsqlStore implements Store, AutoCloseable {
+    private Connection cnn;
+    private static final String PAGE_LINK = "https://career.habr.com/vacancies/java_developer?page=";
+
+    public PsqlStore(Properties cfg) {
+        try {
+            Class.forName(cfg.getProperty("driver-class-name"));
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+        try {
+            cnn = DriverManager.getConnection(
+                    cfg.getProperty("url"),
+                    cfg.getProperty("username"),
+                    cfg.getProperty("password")
+            );
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Properties loadProperties(String prp) {
+        Properties properties = new Properties();
+        try (InputStream in = PsqlStore.class.getClassLoader().getResourceAsStream(prp)) {
+            properties.load(in);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return properties;
+    }
+
+    private Post getPost(ResultSet resultSet) throws SQLException {
+        return (new Post(
+                resultSet.getInt("id"),
+                resultSet.getString("name"),
+                resultSet.getString("link"),
+                resultSet.getString("description"),
+                resultSet.getTimestamp("created").toLocalDateTime()
+        ));
+    }
+
+
+    @Override
+    public void save(Post post) {
+        try (PreparedStatement statement = cnn.prepareStatement("insert into post(name, description,"
+               + "link, created) values (?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS)) {
+            statement.setString(1, post.getTitle());
+            statement.setString(2, post.getDescription());
+            statement.setString(3, post.getLink());
+            statement.setTimestamp(4, Timestamp.valueOf(post.getCreated()));
+            statement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Post> getAll() {
+        List<Post> postList = new ArrayList<>();
+        try (PreparedStatement statement = cnn.prepareStatement("select * from post")) {
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    postList.add(getPost(resultSet));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return postList;
+    }
+
+    @Override
+    public Post findById(int id) {
+        Post post = null;
+        try (PreparedStatement statement = cnn.prepareStatement("select * from post where id = ?")) {
+            statement.setInt(1, id);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    post = getPost(resultSet);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return post;
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (cnn != null) {
+            cnn.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        try (PsqlStore store = new PsqlStore(loadProperties("aggregator.properties"))) {
+            HabrCareerDateTimeParser dateTimeParser = new HabrCareerDateTimeParser();
+            HabrCareerParse careerParse = new HabrCareerParse(dateTimeParser);
+            List<Post> list = careerParse.list(PAGE_LINK);
+            for (Post post: list) {
+                store.save(post);
+            }
+            System.out.println(store.getAll());
+            System.out.println(store.findById(12));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
